3bfaddaa7153b5b150bcdb52bf8a60d9c957fb3e
Peter Mwendia <ptermwendia@gmail.com>
2023-10-27 11:11:06 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-spark-submit --class org.example.lab5.App target/lab5-1.0-SNAPSHOT.jar hdfs:///nasa_19950801.tsv
+# Upload the File to HDFS:
+hadoop fs -copyFromLocal ./nasa_19950801.tsv /user/
+
+spark-submit --class org.example.lab5.App target/lab5-1.0-SNAPSHOT.jar hdfs:///user/nasa_19950801.tsv
 #spark-submit --class org.example.lab5.App target/lab5-1.0-SNAPSHOT.jar hdfs:///nasa_19950801.tsv 2>/dev/null
